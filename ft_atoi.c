d1f1bdcd0732c90ebb36cbf9704d7b42675bdef9
Orapeleng MPUTLE <omputle@c4r12s4.wethinkcode.co.za>
2019-05-20 13:27:05 +0200

    Functions

---
+++ ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/20 09:57:34 by omputle           #+#    #+#             */
+/*   Updated: 2019/05/20 10:50:23 by omputle          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_atoi(const char *str)
+{
+	int	num;
+	int	sign;
+	int	count;
+
+	num = 0;
+	sign = 1;
+	count = 0;
+	if (str[count] == '+' || str[count] == '-')
+	{
+		if (str[count] == '-')
+		{
+			sign = -1;
+		}
+		count++;
+	}
+	while (str[count] >= '0' && str[count] <= '9')
+	{
+		num = num * 10 + (str[count] - '0');
+		count++;
+	}
+	return (num * sign);
+}

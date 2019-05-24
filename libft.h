6f696462b3cd20488628bbfa1ec087f831b6dc73
Orapeleng MPUTLE <omputle@c4r12s4.wethinkcode.co.za>
2019-05-24 17:18:56 +0200

    lib

---
+++ libft.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/24 10:17:33 by omputle           #+#    #+#             */
+/*   Updated: 2019/05/24 10:45:21 by omputle          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdio.h>
+# include <ctype.h>
+# include <string.h>
+# include <stdlib.h>
+
+int		ft_strlen(const char *s);
+int		ft_atoi(const char *str);
+char	*ft_strcpy(char *dst, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+char	*ft_strncpy(char *dest, const char *src, size_t len);
+char	*ft_strcat(char *restrict s1, const char *restrict s2);
+char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
+char	*ft_strdup(const char *s1);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+
+#endif

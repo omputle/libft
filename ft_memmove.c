abe63e84a10d7f05d9308794a49f6dd773fdac8e
Orapeleng MPUTLE <omputle@c4r12s4.wethinkcode.co.za>
2019-06-01 11:30:06 +0200

    file

---
+++ ft_memmove.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/31 10:50:32 by omputle           #+#    #+#             */
+/*   Updated: 2019/06/01 11:23:43 by omputle          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *str1, const void *str2, size_t n)
+{
+	size_t			count;
+	size_t			len_d;
+	unsigned char	*dest;
+	unsigned char	*src;
+
+	count = 0;
+	src = (unsigned char *)str2;
+	dest = (unsigned char *)str1;
+	len_d = ft_strlen(dest);
+	while ((count <= len_d) && (count < n))
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	return (dest);
+}

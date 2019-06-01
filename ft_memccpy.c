b721d92674d21d899d3690caa7c1d7ec249be0d8
Orapeleng MPUTLE <omputle@c4r12s4.wethinkcode.co.za>
2019-06-01 15:59:18 +0200

    file

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,27 +6,27 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 16:18:07 by omputle           #+#    #+#             */
-/*   Updated: 2019/05/31 16:53:34 by omputle          ###   ########.fr       */
+/*   Updated: 2019/06/01 12:27:55 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy(void *restrict dst, const *restrict src, int c, size_t n)
+void	*ft_memccpy(void *restrict s2, const void *restrict s1, int c, size_t n)
 {
-	size_t	count;
-	unsigned char	*s1;
-	unsigned char	*s2;
+	size_t			count;
+	unsigned char	*src;
+	unsigned char	*dest;
 
 	count = 0;
-	s1 = (unsigned char *)src;
-	s2 = (unsigned char *)dst;
-	while (s2[count] && count < n)
+	src = (unsigned char *)s1;
+	dest = (unsigned char *)s2;
+	while (dest[count] && count < n)
 	{
-		s2[count] = s1[count];
-		if (s1[count] == c)
-			return (&s2[count + 1]);
+		dest[count] = src[count];
+		if (src[count] == c)
+			return (&dest[count + 1]);
 		count++;
 	}
-	return (0);
+	return (NULL);
 }
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/01 10:24:07 by omputle           #+#    #+#             */
-/*   Updated: 2019/06/01 10:24:10 by omputle          ###   ########.fr       */
+/*   Updated: 2019/06/01 11:32:31 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	count;
+	size_t			count;
 	unsigned char	*str1;
 	unsigned char	*str2;
 

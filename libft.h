d71ce9186726819364c03ebc5b2b497d89f868d4
Orapeleng MPUTLE <omputle@c4r12s4.wethinkcode.co.za>
2019-06-04 16:43:57 +0200

    Updated

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 10:17:33 by omputle           #+#    #+#             */
-/*   Updated: 2019/06/03 16:39:49 by omputle          ###   ########.fr       */
+/*   Updated: 2019/06/04 16:42:34 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,5 +57,6 @@ void	*ft_memccpy(void *restrict s2, const void *restrict s1, int c, size_t n);
 void	*ft_memcpy(void *str1, const void *str2, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
 void	*ft_memalloc(size_t size);
+char	*ft_strnew(size_t size);
 
 #endif

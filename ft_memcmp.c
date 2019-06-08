84cf395d92f4817e00b2559e0673960c6ccf2294
Orapeleng MPUTLE <omputle@c4r10s2.wethinkcode.co.za>
2019-06-08 16:55:29 +0200

    files

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/01 10:24:07 by omputle           #+#    #+#             */
-/*   Updated: 2019/06/07 16:34:47 by omputle          ###   ########.fr       */
+/*   Updated: 2019/06/08 16:05:42 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,14 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	size_t			count;
 	unsigned char	*str1;
 	unsigned char	*str2;
-	int				ans;
+//	int				ans;
 
 	count = 0;
-	ans = 0;
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	if (n == 0)
 		return (0);
-	while (str1[count] != '\0' || str2[count] != '\0' || count < n)
-	{
-		if (str1[count] != str2[count])
-		{
-			ans = str1[count] - str2[count];
-			break ;
-		}
+	while ((str1[count] == str2[count]) && count < n)
 		count++;
-	}
-	return (ans);
+	return ((int)(str1[count] - str2[count]));
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 10:50:32 by omputle           #+#    #+#             */
-/*   Updated: 2019/06/07 15:18:42 by omputle          ###   ########.fr       */
+/*   Updated: 2019/06/08 15:50:12 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,24 @@
 void	*ft_memmove(void *str1, const void *str2, size_t n)
 {
 	size_t			count;
-	size_t			len_d;
-	size_t			len_s;
 	unsigned char	*dest;
 	unsigned char	*src;
 
-	count = 0;
+	count = n;
 	src = (unsigned char *)str2;
 	dest = (unsigned char *)str1;
-	len_d = ft_strlen((char *)dest);
-	len_s = ft_strlen((char *)src);
 	if (dest == '\0' && src == '\0')
 		return (0);
-	if (len_s < len_d)
+	if (dest > src)
 	{
-		while (count < n)
+		while (count--)
 		{
 			dest[count] = src[count];
-			count++;
 		}
 	}
 	else
 	{
-		while (n > 0)
-		{
-			*(dest++) = *(src++);
-			n--;
-		}
-		
+		dest = ft_memcpy(dest, src, count);
 	}
 	return (dest);
 }

c3593d75b380723508db62993988d867ef9b0984
Orapeleng MPUTLE <omputle@c4r12s5.wethinkcode.co.za>
2019-06-08 11:50:36 +0200

    edits

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:57:34 by omputle           #+#    #+#             */
-/*   Updated: 2019/06/01 10:23:50 by omputle          ###   ########.fr       */
+/*   Updated: 2019/06/08 09:30:22 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,14 @@ int		ft_atoi(const char *str)
 	num = 0;
 	sign = 1;
 	count = 0;
+	while (str[count] && (str[count] == ' ' || str[count] == '\t' ||
+				str[count] == '\f' || str[count] == '\n' || 
+				str[count] == '\v' || str[count] == '\r'))
+		count++;
 	if (str[count] == '+' || str[count] == '-')
 	{
 		if (str[count] == '-')
-		{
 			sign = -1;
-		}
 		count++;
 	}
 	while (str[count] >= '0' && str[count] <= '9')
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 12:25:06 by omputle           #+#    #+#             */
-/*   Updated: 2019/06/07 17:08:27 by omputle          ###   ########.fr       */
+/*   Updated: 2019/06/08 08:54:46 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,21 @@ char	*ft_strrchr(const char *str, int c)
 {
 	int				count;
 	int				len;
-	char	*s;
+	char			*s;
 
 	count = 0;
 	s = (char *)str;
-	while (str[count] != '\0')
+	while (s[count] != '\0')
 		count++;
 	len = count;
-	count = count - 1;
-	while (s[count])
+	count--;
+	while (count >= 0)
 	{
 		if (s[count] == (char)c)
 			return (&s[count]);
 		count--;
 	}
 	if (c == '\0')
-		return ((char *)&str[len]);
+		return (&s[len]);
 	return (0);
 }

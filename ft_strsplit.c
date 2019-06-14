cdafebbcc003c410fd71aec4905884f4197dce2d
Orapeleng MPUTLE <omputle@c4r12s5.wethinkcode.co.za>
2019-06-14 07:52:09 +0200

    Edits

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,15 +6,36 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/13 10:40:06 by omputle           #+#    #+#             */
-/*   Updated: 2019/06/13 10:56:43 by omputle          ###   ########.fr       */
+/*   Updated: 2019/06/13 15:44:39 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+//#include "libft.h"
+#include <stdio.h>
 
-char	**ft_strsplit(char const *s, char c)
+size_t	word_count(char *str, char c)
 {
-	char	**res;
-	size_t	i;
-	size_t	j;
+	size_t	count;
+	size_t	ans;
+
+	ans = 0;
+	count = 0;
+	while (str[count] != '\0')
+	{
+		while (str[count] == c && str[count] != '\0')
+			count++;
+		if (str[count] != c && str[count] != '\0')
+			ans++;
+		while (str[count] != c && str[count] != '\0')
+			count++;
+	}
+	return (ans);
+}
+
+int		main(void)
+{
+	char	str[] = "You think darkness is your ally";
+	char	c = ' ';
+	printf("String: |%s|\t Word count : |%lu|\n", str, word_count(str, c));
+	return (0);
 }

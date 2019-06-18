36f29fa4f86691ce801b2f8463b025c2e62fc01b
Orapeleng MPUTLE <omputle@c4r12s5.wethinkcode.co.za>
2019-06-18 09:01:30 +0200

    Edits

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 09:50:20 by omputle           #+#    #+#             */
-/*   Updated: 2019/05/28 09:55:43 by omputle          ###   ########.fr       */
+/*   Updated: 2019/06/18 08:25:00 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,23 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	num;
-
-	num = 0;
-	if (n < 9 && n > -9)
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else
 	{
-		if (n >= 0)
+		if (n < 0)
 		{
-			num = '0' + n;
+			ft_putchar_fd('-', fd);
+			n = n * -1;
+		}
+		else if (n >= 10)
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putchar_fd(n % 10 + '0', fd);
 		}
 		else
 		{
-			n = n * -1;
-			num = '0' + n;
-			write(fd, "-", 1);
+			ft_putchar_fd(n + '0', fd);
 		}
-		write(fd, &num, 1);
-	}
-	else
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
 	}
 }

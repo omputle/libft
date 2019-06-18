7345be13ff00ec0e0bfad03c36869a2e26504693
Orapeleng MPUTLE <omputle@c4r12s5.wethinkcode.co.za>
2019-06-18 17:14:04 +0200

    Edits

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 09:01:53 by omputle           #+#    #+#             */
-/*   Updated: 2019/06/11 15:54:37 by omputle          ###   ########.fr       */
+/*   Updated: 2019/06/18 17:02:01 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,29 @@ char	*ft_strtrim(char const *s)
 {
 	size_t	start;
 	size_t	end;
-	size_t	size;
 	size_t	count;
 	char	*str;
 
 	start = 0;
 	count = 0;
-	end = ft_strlen(s) - 1;
+	end = 0;
 	if (!s)
-		return (0);
+		return (NULL);
 	while (s[start] == ' ' || s[start] == '\n' || s[start] == '\t')
 		start++;
+	if (s[start] == '\0')
+		return ("");
+	end = ft_strlen(s) - 1;
 	while (s[end] == ' ' || s[end] == '\n' || s[end] == '\t')
 		end--;
-	if (start > end)
-		return (0);
-	size = end - start + 2;
-	str = (char *)malloc(sizeof(char) * (size));
+	str = (char *)malloc(sizeof(char) * (end - start + 2));
 	if (!str)
-		return (0);
-	else
+		return (NULL);
+	while ((start + count) <= end)
 	{
-		while (count < (size - 1))
-		{
-			str[count] = (char)s[start];
-			count++;
-			start++;
-		}
-		str[count] = '\0';
-		return (str);
+		str[count] = s[start + count];
+		count++;
 	}
+	str[count] = '\0';
+	return (str);
 }
